3b9b0f899b724f79d40550cbf008113bf13616de
pensiunanhacker <49938328+pensiunanhacker@users.noreply.github.com>
2019-11-25 14:43:00 +0800

    enc

--- enc.py
+++ enc.py
@@ -1,4 +1,4 @@
-# author : @pensiunanhacker
+# author : @Syhrularv_
 # -*- coding: utf-8 -*-
 
 import os
@@ -15,36 +15,36 @@ Y = '\033[1;33m'
 C = '\033[1;36m'
 
 ask = G + '[' + W + '?' + G + '] '
-NICE = G + '[' + W + '√' + G + '] '
-UNLUCKY = R + '[' + W + '!' + R + ']'
+good = G + '[' + W + '√' + G + '] '
+bakekok = R + '[' + W + '!' + R + ']'
 
 banner = """
-{}         _xxxx_{}        _________________
-{}        xxxxxxxx{}      |                 |
-{}       @p~qp~~qMb{}   ._| {}Bash phencdec {}|
+{}         _nnnn_{}        _________________
+{}        dMMMMMb{}      |                 |
+{}       @p~qp~~qMb{}   ._| {}bash phcode {}|
 {}       M{}({}@{})({}@{}) {}M|{}  /  |_________________|
 {}       @,{}----.{}JM|{}_/
 {}      JS^{}\__/{}  qKL
-     xXX        xXXx
-    xXX          xXXx
-   xXX            XXXx
-   XXX            XXXX    {}Coded by {}: {}pensiunanhacker
-{}  XXX            XXXX    {}Teleg       {}: {}pensiunanhacker
-{} __|'\ .        |\{}pH xXX
-{} |    `.       | `' \{}Xx
+     dMM        MMMb
+    dMM          MMMb
+   MMM            MMMb
+   MMM            MMMM    {}Coded by {}: {}pensiunanhacker
+{}   MMM            MMMM    {}YT       {}: {}pensiunanhacker
+{} __|'\ .        |\{}dS qML
+{} |    `.       | `' \{}Zq
 {}_)      \.{}___.{},|     .'
-\____   ){}RRRRR{}|   .'
+\____   ){}MMMMMM{}|   .'
      `-'       `--'
 """.format(D,W,D,W,D,W,Y,W,D,W,D,W,D,W,D,W,D,Y,D,W,D,Y,D,G,W,G,D,G,W,G,Y,D,Y,D,Y,D,Y,D,Y)
 
 banner2 = """
-   {}[{}1{}]{} ENC      {}[{}2{}]{} DEC
+   {}[{}1{}]{} Encript      {}[{}2{}]{} Decrypt
 """.format(G,W,G,W,G,W,G,W)
 
 print banner
 print banner2
 
-def dekrip():
+def dec1():
    try:
        sc = raw_input(ask + W + "Script " + G + "> " + W)
        f = open(sc,'r')
@@ -58,34 +58,34 @@ def dekrip():
        f.write(newdata)
        f.close()
 
-       os.system("touch p1234.sh")
-       os.system("bash " + out + " > p1234.sh")
+       os.system("touch ph1234.sh")
+       os.system("bash " + out + " > ph1234.sh")
        os.remove(out)
-       os.rename("p1234.sh", out)
-       print (NICE + "Done..")
+       os.rename("ph1234.sh", out)
+       print (good + "Done..")
 
    except KeyboardInterrupt:
-       print (UNLUCKY + " ❌")
+       print (bakekok + " Stopped!")
    except IOError:
-       print (UNLUCKY + " not found🔭")
+       print (bakekok + " File Not Found!")
 
-def enkrip():
+def enc1():
    try:
        script = raw_input(ask + W + "Script " + G + "> " + W)
        output = raw_input(ask + W + "Output " + G + "> " + W)
-       os.system("bash-FVCK " + script + " -o " + output )
-       print (NICE + "Done..")
+       os.system("bash-obfuscate " + script + " -o " + output )
+       print (good + "Done..")
    except KeyboardInterrupt:
-       print (UNLUCKY + " 💢")
+       print (bakekok + " Stopped!")
    except IOError:
-       print (UNLUCKY + " ❌")
+       print (bakekok + " File Not Found!")
 
 
-penhack = raw_input(W + "Choose" + G + " > ")
+code1 = raw_input(W + "Choose" + G + " > ")
 
-if penhack == "1" or penhack == "01":
-   enkrip()
-elif penhack == "2" or penhack == "02":
-   dekrip()
+if code1 == "1" or code1 == "01":
+   enc1()
+elif code1 == "2" or code1 == "02":
+   dec1()
 else:
-   print (UNLUCKY + " ❌")
+   print (bakekok + " Wrong input")
